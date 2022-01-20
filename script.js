e94837746202d16945295a73f0a186283852a9d5
Aman Patel <aman17626@gmail.com>
2022-01-20 19:24:53 +0530

    Completed the visuals and layout of the page

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Welcome to Spotify");
+let songs = [{
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.jpg"
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.jpg"
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.jpg"
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.jpg"
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.jpg"
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.jpg"
+    },
+    {
+        songName: "Salam-e-Ishq",
+        filePath: "song/1.mp3",
+        coverPath: "covers/1.jpg"
+    },
+]
+let audioElement = new Audio('1.mp3');
+audioElement.play();
