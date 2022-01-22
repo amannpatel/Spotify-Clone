3f04d12b5f31651b894f72aa5a6d96e80890fb97
Aman Patel <aman17626@gmail.com>
2022-01-22 17:50:38 +0530

    Updated javaScript file for play pause functionality

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 console.log("Welcome to Spotify");
+
+// Initialize the variable
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
 let songs = [{
         songName: "Salam-e-Ishq",
         filePath: "song/1.mp3",
@@ -35,5 +42,23 @@ let songs = [{
         coverPath: "covers/1.jpg"
     },
 ]
-let audioElement = new Audio('1.mp3');
-audioElement.play();
+// audioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    } else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
+})
+
+// Listen to Events
+myProgressBar.addEventListener('timeupdate', () => {
+    console.log('timeupdate');
+    // Update Seekbar
+})
